d71141093bf11bea20e27c11bcad596258a9efa8
Cyri0 <nemestamas94@gmail.com>
2024-09-24 19:21:28 +0200

    images serve by static

--- backend/desserts/views.py
+++ backend/desserts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .serializers import DessertSerializer
+from .models import Dessert
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(["GET"])
+def getDesserts(request):
+    all_desserts = Dessert.objects.all()
+    # lekérjük az összes desszertet
+    serialized = DessertSerializer(all_desserts, many=True)
+    # serializálunk
+    return Response(serialized.data)
+    # visszaadjuk a serializált adatokat
